c3e3b006abf4872d71c3b067785ac6c9f9372175
kinziq <144268675+kinziq@users.noreply.github.com>
2023-10-10 14:30:00 -0400

    Update sorting.py

--- sorting.py
+++ sorting.py
@@ -31,10 +31,49 @@ def bubbleSort(arr):
             # can just exit the main loop.
             return
 
+def binary_search(arr, low, high, x):
+ 
+    # Check base case
+    if high >= low:
+ 
+        mid = (high + low) // 2
+ 
+        # If element is present at the middle itself
+        if arr[mid] == x:
+            return mid
+ 
+        # If element is smaller than mid, then it can only
+        # be present in left subarray
+        elif arr[mid] > x:
+            return binary_search(arr, low, mid - 1, x)
+ 
+        # Else the element can only be present in right subarray
+        else:
+            return binary_search(arr, mid + 1, high, x)
+ 
+    else:
+        # Element is not present in the array
+        return -1
+
+def search():
+    search = input("Who would you like to search for: ")
+    result = binary_search(namelist, 0, len(namelist)-1, search)
+    if not result == -1:
+        print(f"You have searched for: {search}\n {search} was found in: {result}")
+    else:
+        print(f"{search} was not found.")
+
+def quit():
+    quit = input("If you would like to exit enter a q: \n If you would like to continue")
+    
+    if quit == ("q"):
+        exit()
 
 
-
-
-
-bubbleSort(namelist)
-print(f'namelist')
+run = True
+while run:    
+    
+    bubbleSort(namelist)
+    print(f'{namelist}')
+    search()
+    quit()
